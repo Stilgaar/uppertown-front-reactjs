60f38c78e7ce9e72160126ec140d26176f69843b
Stilgaar <stilgar182@gmail.com>
2021-11-11 19:03:06 +0100

    pouet pouet

--- src/Components/Home/Infos/Infos.js
+++ src/Components/Home/Infos/Infos.js
@@ -11,17 +11,43 @@ function Infos() {
         axios.get("http://localhost:1337/api/users/users")
             .then((res) => setUsers(res.data))
         axios.get("http://localhost:1337/api/announces/allAnnounces")
-        .then((res) => setAnn(res.data))       
-        },[])
+            .then((res) => setAnn(res.data))
+    }, [])
 
 
-    return(
-        <div>
-            <div>Utilisateurs Engeristrés</div>
-            <div>{users.length}</div>
+    let clienttokens = 0;
+    for (let i = 0; i < users.length; i++) { clienttokens += users[i].stableCoins }
 
-            <div>Biens Immobiliers</div>
-            <div>{ann.length}</div>
+    let freetokens = 0;
+    for (let i = 0; i < ann.length; i++) { freetokens += ann[i].share_number }
+
+    return (
+        <div className="infos-container-container">
+
+            <div className="infos-container">
+                <div>Tokens Achetés</div>
+                <div>{clienttokens}</div>
+            </div>
+
+            <div className="infos-container">
+
+                <div>Utilisateurs</div>
+                <div>{users.length}</div>
+            </div>
+
+            <div className="infos-container">
+
+
+                <div>Biens Immobiliers</div>
+                <div>{ann.length}</div>
+            </div>
+
+            <div className="infos-container">
+
+                <div>Tokens Disponible</div>
+                <div>{freetokens}</div>
+
+            </div>
 
         </div>
     )
