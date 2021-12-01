1956ab792ffae9df3e218bbff5fd81b4a0adbdae
Stilgaar <stilgar182@gmail.com>
2021-12-01 15:41:32 +0100

    error.js

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Error() {
     return(
         <div className="erreur">
-            <h2>Error 403 : Forbidden</h2>
+            <h2>Error 401 : Unauthorized </h2>
             <div>Vous vous êtes perdus ? </div>
             <div>Nous vous invitions à vous inscrire sur <Link to='/'><span className="erreur-false-link"> la page d'acceuil </span> </Link> pour pouvoir accèder au reste du site</div>
         </div>
