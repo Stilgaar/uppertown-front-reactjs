d50139fc7e14a5fd21e8dd1bb6ca6207cb6d07c0
Stilgaar <stilgar182@gmail.com>
2022-02-03 13:36:07 +0100

    purge-css
    rajout du package gulp-purgcss et purge du css dans le JSX

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,13 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-// const purgecss = require('gulp-purgecss') // a activer plus tard
+const purgecss = require('gulp-purgecss')
+
 function buildStyles() {
-    return src('SASS-SCSS/**/*.scss', 'Components/**/*.scss')
+    return src('SASS-SCSS/**/*.scss')
         .pipe(sass())
-        //   .pipe(purgecss({ content: ['*.html'] })) à activer plus tard avec le purge
+        .pipe(purgecss({ content: ['../src/**/*.js'] }))
         .pipe(dest('css'))
 }
+
 function watcher() {
-    watch(['SASS-SCSS/**/*.scss', 'Components/**/*.scss'], buildStyles)
+    watch(['SASS-SCSS/**/*.scss', '../src/**/*.js'], buildStyles)
 }
+
 exports.default = series(buildStyles, watcher)
